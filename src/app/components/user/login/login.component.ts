9c301104393cd5dabaf243a0186f54369c4f432c
Safnas <iamsafnas@gmail.com>
2021-01-08 23:20:30 +0530

    login form with very basic validation

--- src/app/components/user/login/login.component.ts
+++ src/app/components/user/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  submitted: boolean = false;
+  loading = false;
+  error = '';
+
+  constructor(private userService: UserService,
+              private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
+
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  loginForm = this.fb.group(
+    {
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    }
+  )
+
+  onSubmit() {
+    this.submitted = true;
+    this.loading = true;
+    if (this.loginForm.invalid) {
+      this.loading = false;
+      return;
+    }
+
+    let user = {
+      email: this.loginForm.get('email')?.value,
+      password: this.loginForm.get('password')?.value,
+    }
+
+    console.log(user)
+  }
+    
+
 
 }
