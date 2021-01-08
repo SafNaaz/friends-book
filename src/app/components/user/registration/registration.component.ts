7ec60433edc653f1eb3e762f08c15d4e87d643ae
Safnas <iamsafnas@gmail.com>
2021-01-08 22:33:23 +0530

    logged user registered data

--- src/app/components/user/registration/registration.component.ts
+++ src/app/components/user/registration/registration.component.ts
@@ -1,59 +1,82 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { DateOfBirthValidator } from 'src/app/helpers/validators/dob-validator/dob.validator';
 import { ConfirmPasswordValidator } from 'src/app/helpers/validators/password-validator/password.validator';
 import { UserRegistration } from 'src/app/models/user/UserRegistration';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+  styleUrls: ['./registration.component.css'],
 })
 export class RegistrationComponent implements OnInit {
-
   submitted: boolean = false;
+  loading = false;
+  error = '';
 
-  user! : UserRegistration;
+  user!: UserRegistration;
 
-  constructor(private fb : FormBuilder) { }
-
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private userService: UserService
+  ) {
+    // if (this.userService.currentUserValue?.token) {
+    //   this.router.navigate(['/']);
+    // }
   }
 
-  get f(){
+  ngOnInit(): void {}
+
+  get f() {
     return this.registrationForm.controls;
   }
 
-  registrationForm = this.fb.group({
-    firstName:['', [Validators.required, Validators.minLength(3)]],
-    lastName:['', [Validators.required, Validators.minLength(3)]],
-    email:['', [Validators.required, Validators.email]],
-    dob:['', [Validators.required]],
-    gender:['', [Validators.required]],
-    password:['', [Validators.required, Validators.minLength(6)]],
-    confirmPassword:['', [Validators.required, Validators.minLength(6)]],
-    agreeTerms:['', Validators.required]
-  },{validators : [ConfirmPasswordValidator.matchPassword,
-                    DateOfBirthValidator.validDob]})
+  registrationForm = this.fb.group(
+    {
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      dob: ['', [Validators.required]],
+      gender: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(6)]],
+      agreeTerms: ['', Validators.required],
+    },
+    {
+      validators: [
+        ConfirmPasswordValidator.matchPassword,
+        DateOfBirthValidator.validDob,
+      ],
+    }
+  );
 
-  onSubmit(){
+  onSubmit() {
     this.submitted = true;
-    if(this.registrationForm.invalid){
+    if (this.registrationForm.invalid) {
       return;
     }
 
-    this.user = new UserRegistration(this.registrationForm.get('firstName')?.value,
-    this.registrationForm.get('lastName')?.value,
-    this.registrationForm.get('email')?.value,
-    this.registrationForm.get('gender')?.value,
-    this.registrationForm.get('dob')?.value,
-    this.registrationForm.get('password')?.value,
-      
-    )
+    this.user = new UserRegistration(
+      this.registrationForm.get('firstName')?.value,
+      this.registrationForm.get('lastName')?.value,
+      this.registrationForm.get('email')?.value,
+      this.registrationForm.get('gender')?.value,
+      this.registrationForm.get('dob')?.value,
+      this.registrationForm.get('password')?.value
+    );
 
-    this.registrationForm.reset()
+    this.userService.login(this.user);
+
     
-    console.log(this.user)
-  }
+    // this.error = "failed";
+    this.loading = true
 
+    this.registrationForm.reset();
+    // return
+
+    this.router.navigate(['/login']);
+  }
 }
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { UserRegistration } from '../models/user/UserRegistration';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  baseUrl = 'https://nodejs-fb-app.herokuapp.com';
+
+  constructor(private http : HttpClient) { }
+
+  login(user: UserRegistration) {
+    this.http.post(`${this.baseUrl}/users/register`, user).subscribe(data =>{
+      console.log(data)
+    })
+  }
+
+}
