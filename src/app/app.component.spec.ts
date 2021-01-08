160620972ca2c81d5ff5e74448f5f08a5d0924cc
Safnas <iamsafnas@gmail.com>
2021-01-08 15:04:00 +0530

    removed boiler plate codes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('friends-book');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('friends-book app is running!');
-  });
 });
