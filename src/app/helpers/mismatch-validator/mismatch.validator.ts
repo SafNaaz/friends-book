dc884a1836aecd24fb6ea0f46e48337a1e11e537
Safnas <iamsafnas@gmail.com>
2021-01-08 17:59:19 +0530

    added regex validation for password

--- src/app/helpers/mismatch-validator/mismatch.validator.ts
+++ src/app/helpers/mismatch-validator/mismatch.validator.ts
@@ -6,6 +6,18 @@ export class ConfirmPasswordValidator{
         let password = control.get('password')?.value;
         let confirmPassword = control.get('confirmPassword')?.value;
 
+        let hasNumber = /\d/.test(password);
+        let hasUpper = /[A-Z]/.test(password);
+        let hasLower = /[a-z]/.test(password);
+        let hasSpecial = /(?=.*[$@!%*?&])/.test(password);
+        // console.log('Num, Upp, Low', hasNumber, hasUpper, hasLower);
+        const valid = hasNumber && hasUpper && hasLower && hasSpecial;
+        if (!valid) {
+            // return what´s not valid
+            control.get('password')?.setErrors({passwordWeak: true})
+            return { strong: true };
+        }
+
         if(password != confirmPassword){
             control.get('confirmPassword')?.setErrors({passwordMismatch: true})
             return ({not_the_same: true});
