17da4c782b95e9cf4e34f71e9144770bf55d33b8
Safnas <iamsafnas@gmail.com>
2021-01-11 13:47:03 +0530

    skeleton for posts page

--- src/app/social/social/social.component.ts
+++ src/app/social/social/social.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-social',
@@ -7,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SocialComponent implements OnInit {
 
-  constructor() { }
+  currentUser: User = new User;
+
+  constructor(private userService: UserService) { 
+    this.userService.currentUser.subscribe(x => this.currentUser = x);
+  }
 
   ngOnInit(): void {
   }
