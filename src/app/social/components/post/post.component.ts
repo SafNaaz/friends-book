8e921dbfa8af6eb5c3c6b6ceb882946d591848e3
Safnas <iamsafnas@gmail.com>
2021-01-11 18:44:08 +0530

    loading post image and user image

--- src/app/social/components/post/post.component.ts
+++ src/app/social/components/post/post.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { Post } from '../../models/post';
+import { PostService } from '../../services/post.service';
 
 @Component({
   selector: 'app-post',
@@ -10,9 +12,33 @@ export class PostComponent implements OnInit {
 
   @Input() post! : Post;
 
-  constructor() { }
+  postImage : any;
+  userImage : any;
+
+  postImageLoaded: boolean = false;
+  userImageLoaded: boolean = false;
+
+  constructor(private postService: PostService,
+              private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
+    this.getPostImage();
   }
+
+  getPostImage(){
+    this.postService.getImage(this.post.postImageId).subscribe(blob => {
+     var urlCreator = window.URL;
+     this.postImage = this.sanitizer.bypassSecurityTrustUrl(urlCreator.createObjectURL(blob));
+     this.postImageLoaded = true;
+    })
+   }
+
+   getUserImage(){
+    this.postService.getImage(this.post.userPhotoId).subscribe(blob => {
+     var urlCreator = window.URL;
+     this.userImage = this.sanitizer.bypassSecurityTrustUrl(urlCreator.createObjectURL(blob));
+     this.userImageLoaded = true;
+    })
+   }
 
 }
