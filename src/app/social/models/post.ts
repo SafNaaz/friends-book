b2bc11b0b50b94e8002c19fd633f58aeb411ae37
Safnas <iamsafnas@gmail.com>
2021-01-11 17:07:19 +0530

    showing all posts

---
+++ src/app/social/models/post.ts
@@ -0,0 +1,12 @@
+export class Post {
+    isActive!: boolean;
+    post!: string;
+    userId!: string;
+    userName!: string;
+    userPhotoId!: string;
+    isAdmin!: boolean;
+    profession!: string;
+    createdDate!: string;
+    id!: string;
+    postImageId!: string;
+}
--- src/app/social/services/post.service.ts
+++ src/app/social/services/post.service.ts
@@ -1,16 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Post } from '../models/post';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PostService {
-
   baseUrl = 'https://nodejs-fb-app.herokuapp.com';
 
-  constructor(private http : HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getUserImage(photoId: string){
-  return this.http.get(`${this.baseUrl}/files/`+photoId, {responseType: 'blob'})
-}
+  getUserImage(photoId: string) {
+    return this.http.get(`${this.baseUrl}/files/` + photoId, {
+      responseType: 'blob',
+    });
+  }
+
+  getAllPosts() {
+    return this.http.get<Post[]>(`${this.baseUrl}/posts`);
+  }
 }
